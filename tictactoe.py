707d77713edeb403d7e57659ad3ca93243393ac0
Sersovi <sersovi@pm.me>
2019-04-07 08:38:55 +0200

    Actualización de rows y lines

--- tictactoe.py
+++ tictactoe.py
@@ -37,19 +37,25 @@ def create_board(size):
 def check_line(x):
     for i in range(3):
         if x[i][0] == x[i][1] == x[i][2]:
-            while x[i][0] != 0:
+            if x[i][0] != 0:
                 print(x[i][0], " wins bcs of line!")
                 break
-        else:
-            print("Not in lines")
+            else:
+                continue
+            break
+    else:
+        print("Not in lines")
 
 
 def check_row(x):
     for i in range(3):
-        if x[1][i] == x[0][i] == x[2][i]:
-            while x[0][i] != 0:
+        if x[0][i] == x[1][i] == x[2][i]:
+            if x[0][i] != 0:
                 print(x[i][0], " wins bcs of row!")
                 break
+            else:
+                continue
+            break
     else:
         print("Not in rows.")
 
@@ -57,11 +63,10 @@ def check_row(x):
 
 def check_diagonal(x):
     if x[0][0] == x[1][1] == x[2][2] or x[0][2] == x[1][1] == x[2][0]:
-        while x[1][1] != 0:
+        if x[1][1] != 0:
             print(x[1][1], " wins bcs of diagonal!")
-            break
-        else:
-            print("Not in diagonal.")
+    else:
+        print("Not in diagonal.")
 
 
 def check_all(x):
